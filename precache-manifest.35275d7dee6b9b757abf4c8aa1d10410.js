5f93c578e94ff995db043dd180af15d5021b5098
anishs95 <anish.s95@gmail.com>
2021-05-25 22:52:02 +0530

    Updates

---
+++ precache-manifest.35275d7dee6b9b757abf4c8aa1d10410.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8f31c2f046935876b2957fa781c30d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8b3bf7f1df21b4e7128",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b3bf7f1df21b4e7128",
+    "url": "/static/js/main.a437eb52.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
